9dca9ea2502c029b1d043188325bc9065e18269f
Kirstin Veltman <kirstin@veltman.co>
2018-06-14 16:44:52 -0700

    Add project files.

---
+++ Abstract_Classes_Exercises1/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes_Exercises1
+{
+    class Employee : Person
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+    }
+}
---
+++ Abstract_Classes_Exercises1/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes_Exercises1
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public virtual void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+    }
+}
